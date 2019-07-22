854338edf0b64b02a0156ddee701642f170ae7c3
sivasankar <sivasankar.vadlamani@pennywisesolutions.com>
2019-07-22 18:20:43 +0530

    second commit

---
+++ src/main/java/com/jenikns/TestJenkins/App.java
@@ -0,0 +1,13 @@
+package com.jenikns.TestJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
